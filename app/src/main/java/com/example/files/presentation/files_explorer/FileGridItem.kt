e7c9cbfd61b9753e0367837b6594fef628c62899
jacobel640 <jacobel640@gmail.com>
2026-06-04 04:16:21 +0300

    replace XML icons with MD3

--- app/src/main/java/com/example/files/presentation/files_explorer/FileGridItem.kt
+++ app/src/main/java/com/example/files/presentation/files_explorer/FileGridItem.kt
@@ -1,7 +1,6 @@
 package com.example.files.presentation.files_explorer
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -13,18 +12,19 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.CheckCircle
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.files.R
 import com.example.files.models.JFile
 
 @OptIn(ExperimentalFoundationApi::class)
@@ -63,10 +63,11 @@ fun FileGridItem(
                         .background(MaterialTheme.colorScheme.primaryContainer.copy(alpha = 0.7f)),
                     contentAlignment = Alignment.Companion.Center
                 ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.check_circle),
+                    Icon(
+                        imageVector = Icons.Rounded.CheckCircle,
                         contentDescription = "Selected",
-                        modifier = Modifier.Companion.size(24.dp)
+                        tint = MaterialTheme.colorScheme.primary,
+                        modifier = Modifier.size(24.dp)
                     )
                 }
             }
--- app/src/main/java/com/example/files/presentation/files_explorer/FileRowItem.kt
+++ app/src/main/java/com/example/files/presentation/files_explorer/FileRowItem.kt
@@ -1,26 +1,40 @@
 package com.example.files.presentation.files_explorer
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.CheckCircle
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.compositeOver
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.files.R
 import com.example.files.models.JFile
+
+val ColorScheme.selectedItemColor: Color
+    @Composable
+    get() = primaryContainer.copy(alpha = 0.5f).compositeOver(background)
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -31,10 +45,9 @@ fun FileRowItem(
     onLongClick: () -> Unit,
     iconContent: @Composable () -> Unit
 ) {
-    val context = LocalContext.current
     Column(
         modifier = Modifier.background(
-            if (isSelected) MaterialTheme.colorScheme.primaryContainer.copy(alpha = 0.5f)
+            if (isSelected) MaterialTheme.colorScheme.selectedItemColor
             else MaterialTheme.colorScheme.surface
         )
     ) {
@@ -55,9 +68,10 @@ fun FileRowItem(
                             .background(MaterialTheme.colorScheme.primaryContainer.copy(alpha = 0.7f)),
                         contentAlignment = Alignment.Center
                     ) {
-                        Image(
-                            painter = painterResource(id = R.drawable.check_circle),
+                        Icon(
+                            imageVector = Icons.Rounded.CheckCircle,
                             contentDescription = "Selected",
+                            tint = MaterialTheme.colorScheme.primary,
                             modifier = Modifier.size(24.dp)
                         )
                     }
